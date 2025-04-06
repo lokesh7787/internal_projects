74e6117d803357ed30ac6aec07feb2dc73dbf69e
lokesh <lokesh7787@gmail.com>
2025-04-06 16:42:52 +0530

    initial stage commit

---
+++ parkingslots/src/main/java/com/parkingSlots/parkingslots/ParkingslotsApplication.java
@@ -0,0 +1,13 @@
+package com.parkingSlots.parkingslots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingslotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingslotsApplication.class, args);
+	}
+
+}
---
+++ parkingslots/src/test/java/com/parkingSlots/parkingslots/ParkingslotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkingSlots.parkingslots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingslotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
